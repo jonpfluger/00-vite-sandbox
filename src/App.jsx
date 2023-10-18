0191ba0e66311ce7c1b39c9b96fdf86800250487
jonpfluger <jonpfluger718@gmail.com>
2023-10-17 19:09:09 -0500

    multiple cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,41 @@
 import Card from './components/Card'
 
-const user = {
-  name: "Jon",
-  age: 31,
-  hasPet: true,
-  avatar_url: 'https://placehold.co/100',
-}
+const users = [
+  {
+    name: "Jon",
+    age: 31,
+    hasPet: true,
+    avatar_url: 'https://placehold.co/100',
+  },
+  {
+    name: "Max",
+    age: 33,
+    hasPet: false,
+    avatar_url: 'https://placehold.co/100',
+  },
+  {
+    name: "Josh",
+    age: 35,
+    hasPet: true,
+    avatar_url: 'https://placehold.co/100',
+  },
+]
 
 function App() {
   return (
     <div className="container">
       <h1>Hello World!</h1>
-      <Card
-        name={user.name}
-        age={user.age}
-        hasPet={user.hasPet}
-        avatar_url={user.avatar_url}
-      />
+
+      {users.map(user => {
+        return (
+          <Card
+            name={user.name}
+            age={user.age}
+            hasPet={user.hasPet}
+            avatar_url={user.avatar_url}
+          />
+        )
+      })}
     </div>
   )
 }
