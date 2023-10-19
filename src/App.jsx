b67271f716adefb417c9dcab1bd6855416269584
jonpfluger <jonpfluger718@gmail.com>
2023-10-19 18:35:58 -0500

    clear

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,8 @@
-import { useState, useEffect } from 'react'
-import Card from './components/Card'
-import Form from './components/Form'
-import { fetchGithubUser } from './utils/github'
+
 
 function App() {
-  const [username, setUsername] = useState('jonpfluger')
-  const [user, setUser] = useState(null)
-
-  useEffect(() => {
-    fetchGithubUser(username)
-      .then(json => setUser(json))
-  }, [])
-
-  const handleInputChange = e => {
-    setUsername(e.target.value)
-    fetchGithubUser(username)
-      .then(json => setUser(json))
-  }
-
-  const handleSubmit = e => {
-    e.preventDefault()
-  }
-
   return (
-    <div className="container">
-      <Form
-        username={username}
-        handleInputChange={handleInputChange}
-        handleSubmit={handleSubmit}
-      />
-
-      {user ? (
-        <Card 
-          name={user.name}
-          avatar_url={user.avatar_url}
-        />
-      ) : (
-        <p>No user found.</p>
-      )}
-
-    </div>
+    null
   )
 }
 
--- src/utils/github.js
+++
@@ -1,5 +0,0 @@
-export const fetchGithubUser = async username => {
-  const response = await fetch(`https://api.github.com/users/${username}`)
-  const json = await response.json()
-  return json
-}
