e08703f51755b41c8a94c4358953a96825a79250
jonpfluger <jonpfluger718@gmail.com>
2023-10-19 19:05:12 -0500

    manually routing with useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
-
+import { useState } from 'react'
+import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'
 
 const App = () => {
+  const [page, setPage] = useState('home')
+
+  function renderPage() {
+    if (page === 'home') {
+      return <Home />
+    } else if (page === 'about') {
+      return <About />
+    } else if (page === 'contact') {
+      return <Contact />
+    } else {
+      return <p>404 - Not found.</p>
+    }
+  }
+
   return (
     <>
-      
+      <nav className="d-flex justify-content-center gap-3 bg-dark p-3">
+        <a href="#" className="text-light" onClick={() => setPage('home')}>Home</a>
+        <a href="#" className="text-light" onClick={() => setPage('about')}>About</a>
+        <a href="#" className="text-light" onClick={() => setPage('contact')}>Contact</a>
+      </nav>
+    
+      <div className="container">
+        {renderPage()}
+      </div>
     </>
   )
 }
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,8 @@
 const Contact = () => {
   return (
     <>
-      Contact
+      <h1>Contact</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto sint officiis laboriosam repellat harum vel corporis in dolore quas. Voluptas, labore? Iste cupiditate obcaecati architecto consectetur ad repellat neque in odio illum est totam commodi dolore eaque ullam sint ducimus repellendus libero inventore non autem sunt iure, natus dolores! Sequi.</p>
     </>
   )
 }
